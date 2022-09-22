718e8504622feeccd952a79710ff953cbe4ad1e8
SulmanTariq <SulmanTariq@DESKTOP-FUGQNF4>
2022-09-22 17:04:47 +0500

    Add project files.

---
+++ SBSender/Services/QueueService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System.Text;
+using System.Text.Json;
+
+namespace SBSender.Services
+{
+    public class QueueService : IQueueService
+    {
+        private readonly IConfiguration config;
+
+        public QueueService(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public async Task SendMessageAsync<T>(T serviceBusMessage, string queueName)
+        {
+            var queueClient = new QueueClient(config.GetConnectionString("AzureServiceBus"), queueName);
+            string messageBody = JsonSerializer.Serialize(serviceBusMessage);
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            await queueClient.SendAsync(message);
+        }
+    }
+}
